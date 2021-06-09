425066f98b8169f10357368968d440ddcd2ec88f
Abhishek <abhisheksawoo23@gmail.com>
2021-06-09 16:37:40 +0530

    This is commit from new save

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -5,6 +5,10 @@ public class LoginPage {
 	public void loginPage() {
 		
 		System.out.println("Login Page");
+		System.out.println("Login Page");
+		System.out.println("Login Page");
+		System.out.println("Login Page");
+
 		
 	}
 
